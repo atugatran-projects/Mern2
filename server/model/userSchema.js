fa318730dc45e5f72974930335cbdffe2ed2209c
Atugatran <atugatran@gmail.com>
2022-12-23 23:44:20 +0530

    password hash

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,32 +1,59 @@
-const mongooose = require('mongoose');
+const mongooose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongooose.Schema({
-    name: {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cpassword: {
+    type: String,
+    required: true,
+  },
+  tokens: [
+    {
+      token: {
         type: String,
-        required:true
+        required: true,
+      }
     },
-    email: {
-         type: String,
-        required:true
-    },
-    phone: {
-        type: Number,
-        required:true
-    },
-    // work: {
-    //      type: String,
-    //     required:true
-    // },
-    password: {
-         type: String,
-        required:true
-    },
-    cpassword: {
-         type: String,
-        required:true
-    }
-})
+  ],
+});
+// Passworh Hassing
+userSchema.pre("save", async function (next) {
+  console.log("Hi this is Middleware");
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 13);
+    this.cpassword = await bcrypt.hash(this.cpassword, 13);
+  }
+  next();
+});
 
-const User = mongooose.model('USER', userSchema);
+// Generte Tokken
+userSchema.methods.generateAuthToken = async function () { 
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const User = mongooose.model("USER", userSchema);
 
 module.exports = User;
--- server/router/routh.js
+++ server/router/routh.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 require("../db/conn");
 const User = require("../model/userSchema");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+// Root Req
 router.get("/", (req, res) => {
-  res.send(`Hello world from the server rotuer js`);
+  res.send(`Hello world from the server router.js`);
 });
 
+// Register Req
 router.post("/register", async (req, res) => {
   // Send Message
   // console.log(req.body);
@@ -16,18 +20,59 @@ router.post("/register", async (req, res) => {
   const { name, email, phone, password, cpassword } = req.body;
   if (!name || !email || !phone || !password || !cpassword) {
     return res.status(422).json({ error: "Hi Plz fill the field" });
-  } 
+  }
   try {
+    // If User Exist
     const userExist = await User.findOne({ email: email });
     if (userExist) {
       return res.status(422).json({ error: "User Exist" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ err: "Password do not Match" });
+    } else {
+      // Create New User
+      const user = new User({ name, email, phone, password, cpassword });
+      // Database Save Data
+      const userRegister = await user.save();
+      // If userRegister or Not
+      if (userRegister) {
+        return res.status(200).json({ Succcess: "User Register Successfully" });
+      } else {
+        return res.status(422).json({ error: "User Not Register" });
+      }
     }
-    const user = new User({ name, email, phone, password, cpassword });
-    const userRegister = await user.save();
-    if (userRegister) {
-      return res.status(200).json({ Succcess: "User Register Successfully" });
-    }else{
-      return res.status(422).json({ error: "User Not Register" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Login Req
+router.post("/signin", async (req, res) => {
+  // Send Message
+  // console.log(req.body);
+  // res.json({ message: req.body });
+  // **********************************************
+  // Save Data to Database
+  try {
+    const { email, password } = req.body;
+    // If Filled Empty
+    if (!email || !password) {
+      return res.status(422).json({ error: "Hi Plz fill the field" });
+    }
+    // its Password Hash
+    // if Email Exist in Databasr
+    const userLogin = await User.findOne({ email: email });
+
+    if (userLogin) {
+      // check Password
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+      const token = await userLogin.generateAuthToken();
+      if (!isMatch) {
+        return res.status(400).json({ error: "Invalid Credienials" });
+      } else {
+        return res.status(200).json({ Message: "User Login" });
+      }
+    } else {
+      return res.status(400).json({ error: "Invalid Credienials" });
     }
   } catch (error) {
     console.log(error);
