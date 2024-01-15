49075526629cb7bcbd4b9ec1f08c27d366ec5ab9
Atugaran <atul22g857086@gmail.com>
2024-01-15 22:49:29 +0530

    remove ghpage 13

--- server/app.js
+++ server/app.js
@@ -4,9 +4,17 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://mernloginfrontend.onrender.com');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    next();
+});
+
 // CORS Configuration
 app.use(cors({
-    origin: ['https://mernloginfrontend.onrender.com/','http://localhost:3000'],
+    origin: 'https://mernloginfrontend.onrender.com',
+    // origin: 'http://localhost:3000',
     methods: ['GET', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
 }));
